1f118f9bbff22a4047e046bb8c241c6baec497cd
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-08-09 13:54:29 +0300

    Create board function

--- tictactoe.py
+++ tictactoe.py
@@ -10,6 +10,19 @@ class TicTacToe(tk.Tk):
         self.resizable(width=False, height=False)
         self.Board()
 
+    def Board(self):
+        for i in range(0, 3):
+            row = []
+            for j in range(0, 3):
+                row.append(tk.Button(self, width=10, height=3, font="Arial 35 bold",
+                                     command=lambda x=i, y=j: self.Turn_Taken(x, y)))
+                row[j].grid(row=i, column=j)
+            self.buttons.append(row)
+        tk.Button(self, text="New Game", width=10, height=1, font="Arial 35 bold",
+                  bg="black", fg="gray", activeforeground="white", activebackground="blue",
+                  command=self.New_Game).grid(row=3, column=1)
+
+
 
 
 TicTacToe().mainloop()
